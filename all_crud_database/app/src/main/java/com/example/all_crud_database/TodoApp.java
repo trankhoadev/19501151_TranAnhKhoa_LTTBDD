2371dbbb817a1d13fc2cc466ef1590fcf79b6377
Trần Anh Khoa <trankhoadev@gmail.com>
2022-05-09 16:19:39 +0700

    Accomplish TodoApp screen

---
+++ all_crud_database/app/src/main/java/com/example/all_crud_database/TodoApp.java
@@ -0,0 +1,14 @@
+package com.example.all_crud_database;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TodoApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo_app);
+    }
+}
